a899b6e401b57e040f936a09865ed9459558b78c
Martin Gundtoft <marg@kea.dk>
2021-11-04 07:44:36 +0100

    Rudimentary context implemented. Navigation menu implemented (beta)

---
+++ src/components/generic/Navbar.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { COLORS } from "../../utils/helpers";
+import styled from "styled-components";
+
+const NavList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0.5rem 0;
+  overflow: hidden;
+  border-radius: 0.3rem 0.3rem 0rem 0rem;
+  width: 20rem;
+
+  a {
+    color: ${COLORS.text};
+    text-decoration: none;
+  }
+  .Stopwatch,
+  .Countdown,
+  .XY,
+  .Tabata {
+    background-color: ${COLORS.nav};
+    transition: 0.5s;
+  }
+  .Stopwatch:hover {
+    background-color: ${COLORS.stopwatch};
+  }
+  .Countdown:hover {
+    background-color: ${COLORS.countdown};
+  }
+  .Tabata:hover {
+    background-color: ${COLORS.tabata};
+  }
+  .XY:hover {
+    background-color: ${COLORS.xy};
+  }
+
+  li {
+    display: inline;
+    list-style-type: none;
+    text-decoration: none;
+    padding: 0.75rem;
+  }
+  .current {
+    .Stopwatch {
+      background-color: ${COLORS.stopwatch};
+    }
+    .Countdown {
+      background-color: ${COLORS.countdown};
+    }
+    .XY {
+      background-color: ${COLORS.xy};
+    }
+    .Tabata {
+      background-color: ${COLORS.tabata};
+    }
+  }
+  .active {
+    background-color: #04aa6d;
+  }
+`;
+
+const links = [
+  { name: "Stopwatch", path: "/" },
+  { name: "Countdown", path: "/Countdown" },
+  { name: "XY", path: "/XY" },
+  { name: "Tabata", path: "/Tabata" },
+];
+
+const Navbar = () => {
+  return (
+    <NavList>
+      {links.map((link, index) => (
+        <NavLink key={index} to={link.path} exact activeClassName="current">
+          <li className={link.name}>{link.name}</li>
+        </NavLink>
+      ))}
+    </NavList>
+  );
+};
+export default Navbar;
